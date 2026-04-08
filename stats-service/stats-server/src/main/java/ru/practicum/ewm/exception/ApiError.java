1aa118a106cacc44434b66fb7ef73cc750da08c3
AlexD1533 <zeed1533@yandex.ru>
2026-04-08 16:53:43 +0300

    add stats exceptionHandler

---
+++ stats-service/stats-server/src/main/java/ru/practicum/ewm/exception/ApiError.java
@@ -0,0 +1,12 @@
+package ru.practicum.ewm.exception;
+
+import java.util.List;
+
+public record ApiError(
+        List<String> errors,
+        String message,
+        String reason,
+        String status,
+        String timestamp
+) {
+}
